72688f969cc1bd19d612f5a6b8ca3df480cf6f6c
bradyoo12 <yoohoony@hotmail.com>
2024-02-24 23:46:54 +0900

    yay

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 def hello_world():
     """Example Hello World route."""
     name = os.environ.get("NAME", "World")
-    return f"Hello {name}!"
+    return f"Hello {name}! yay!"
 
 
 if __name__ == "__main__":
